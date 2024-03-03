4d5399c45b40e1f05edfa66a37fb76e29f06bc55
Paul <pauligbinedion@Pauls-MBP-657.cg.shawcable.net>
2024-03-03 02:34:24 -0700

    Week 6 Simulation lab assignment

--- sample.c
+++ sample.c
@@ -1,22 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
-#define BUFSIZE 256
-
-// This program prints the size of a specified file in bytes
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-        return 1;
-    }
-
-    char* cmd[] = { "wc", "-c", argv[1], NULL };
-    execve("/usr/bin/wc", cmd, NULL);
-
-    perror("execve");
-    return 1;
+int main(int argc, char **argv) {
+char *items[] = {"boat", "car", "truck", "train"};
+int index = atoi(argv[1]);
+printf("You selected %s\n", items[index-1]);
 }
